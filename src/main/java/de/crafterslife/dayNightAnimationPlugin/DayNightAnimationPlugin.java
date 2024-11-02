2727904952b5004c4543c05861a3490559b70c1f
Das_leo_ <lenny14032005@gmail.com>
2024-11-02 22:16:10 +0100

    Initial commit

---
+++ src/main/java/de/crafterslife/dayNightAnimationPlugin/DayNightAnimationPlugin.java
@@ -0,0 +1,17 @@
+package de.crafterslife.dayNightAnimationPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DayNightAnimationPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
